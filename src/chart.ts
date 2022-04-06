20da28307de518d6e73f0ee197d2e715187a723a
aditeyapandey <aditeyapandey@gmail.com>
2022-04-06 11:53:45 -0400

    first commit

---
+++ src/chart.ts
@@ -0,0 +1,81 @@
+
+import d3 = require("d3");
+
+export class Barchart {
+  width: number;
+  height: number;
+  containerElement: string;
+
+  constructor(w: number, h: number, container: string) {
+    this.width = w;
+    this.height = h;
+    this.containerElement = container;
+  }
+
+  createChart() {
+      console.log(this.containerElement)
+    const svg = d3
+      .select("."+this.containerElement)
+      .append("svg")
+      .attr("width", this.width)
+      .attr("height", this.height)
+      .append("g")
+      .attr("transform", "translate(40,0)"); // bit of margin on the left = 40
+
+    d3.json(
+      "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_dendrogram.json"
+    ).then((data: any) => {
+      // Create the cluster layout:
+      const cluster = d3.cluster().size([this.height, this.width - 100]); // 100 is the margin I will have on the right side
+
+      // Give the data to this cluster layout:
+      const root = d3.hierarchy(data, function (d: any) {
+        return d.children;
+      });
+      cluster(root);
+
+      // Add the links between nodes:
+      svg
+        .selectAll("path")
+        .data(root.descendants().slice(1))
+        .join("path")
+        .attr("d", function (d: any) {
+          return (
+            "M" +
+            d.y +
+            "," +
+            d.x +
+            "C" +
+            (d.parent.y + 50) +
+            "," +
+            d.x +
+            " " +
+            (d.parent.y + 150) +
+            "," +
+            d.parent.x + // 50 and 150 are coordinates of inflexion, play with it to change links shape
+            " " +
+            d.parent.y +
+            "," +
+            d.parent.x
+          );
+        })
+        .style("fill", "none")
+        .attr("stroke", "#ccc");
+
+      // Add a circle for each node.
+      svg
+        .selectAll("g")
+        .data(root.descendants())
+        .join("g")
+        .attr("transform", function (d: any) {
+          return `translate(${d.y},${d.x})`;
+        })
+        .append("circle")
+        .attr("r", 7)
+        .style("fill", "#69b3a2")
+        .attr("stroke", "black")
+        .style("stroke-width", 2);
+    });
+  }
+}
+
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import $ = require("jquery");
+import { Barchart } from './chart';
+
+
+function sum(x:number, y:number):number {
+    return x + y;
+}
+
+window.onload = () =>{
+    $( ".input" ).append( `<p>${sum(2,5)}</p>` );
+    let barchart1 = new Barchart(460, 460, "input")
+    barchart1.createChart();
+}
